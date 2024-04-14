a9e2d5e1f237bcb9ba26c2d3b60972060dbb7487
VictorRafaelzisfossato <166776046+VictorRafaelzisfossato@users.noreply.github.com>
2024-04-14 09:30:37 -0300

    Add files via upload

---
+++ node.js
@@ -0,0 +1,25 @@
+OlaMundo
+print(educacao);
+<script> .
+{
+  "compilerOptions": {
+    "module": "CommonJS",
+    "target": "ES6"
+  },
+  "exclude": ["node_modules"]
+}
+
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Launch index.html",
+            "type": "firefox",
+            "request": "launch",
+            "reAttach": true,
+            "file": "${workspaceFolder}/index.html"
+        }
+    ]
+}
+
+
